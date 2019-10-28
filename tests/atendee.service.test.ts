be1e018a0fe3dae685fc06d24904b20e3b7ef03e
HerculesGit <hercules.silva@dce.ufpb.br>
2019-10-28 15:04:42 -0300

    Writing Test 03 - Create Atendee with duplicated name

--- tests/atendee.service.test.ts
+++ tests/atendee.service.test.ts
@@ -2,14 +2,20 @@ import { AtendeeService } from "../src/atendee/atendee.service";
 import { Atendee } from "../src/atendee/atendee";
 
 describe('AtendeeServiceTest', () => {
+
+    let service: AtendeeService;
+
+    // This function is called *before each test specification*, *it* function, has been run.
+    beforeEach(() => {
+        service = new AtendeeService();
+    });
+
     it('t01_createAtendee', function () {
 
         // create object
         let atendee = new Atendee();
         atendee.name = 'A name';
 
-        // create service
-        let service = new AtendeeService();
         const createdAtendee = service.create(atendee);
 
         expect(createdAtendee.id).not.toBeUndefined;
@@ -22,13 +28,36 @@ describe('AtendeeServiceTest', () => {
     });
     it('t02_createAtendeeWithoutName ', () => {
         let atendee = new Atendee();
-        let service = new AtendeeService();
 
         try {
             service.create(atendee);
             fail('Accepted atendee without name');
         } catch (ex) {
             expect('Name is mandatory').toEqual(ex.message);
+        }
+    });
+
+    it('t03_atendeeNameDuplicated', () => {
+
+        const aName: string = 'Hermanoteu';
+
+        // creating first atendee
+        let atendee1 = new Atendee();
+        atendee1.name = aName;
+
+        // creating second atendee
+        let atendee2 = new Atendee();
+        atendee2.name = aName; // The same name!
+
+        service.create(atendee1);
+        const failMessage: string = 'Test failed because the system accepted to create atendee with duplicated name';
+        const expectedExceptionMessage: string = "Atendee name cannot be duplicated";
+
+        try {
+            service.create(atendee2);
+            fail(failMessage);
+        } catch (ex) {
+            expect(expectedExceptionMessage).toEqual(ex.message);
         }
     });
 });
