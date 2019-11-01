bde2ed4a5caff833764c265323004af8f90d3e99
HerculesGit <hercules.silva@dce.ufpb.br>
2019-11-01 11:13:31 -0300

    Writing Test 12 - Update Atendee with invalid email

--- tests/atendee.service.test.ts
+++ tests/atendee.service.test.ts
@@ -194,5 +194,23 @@ describe('AtendeeServiceTest', () => {
         expect(creationDate).toEqual(updatedAtendee.date);
     });
 
+    it('t12_updateAtendeeWithInvalidEmail', () => {
+
+        let atendee: Atendee = serviceHelper.createAtendee(service, EXAMPLE_NAME, EXAMPLE_EMAIL, EXAMPLE_SSN);
+
+        const failMessage = "Test failed because the system accepted to update atendee with invalid e-mail format";
+        const expectedExceptionMessage = "Atendee e-mail format is invalid";
+
+        atendee.email = 'ssdd@.dd';
+        serviceHelper.tryCreateAtendeeWithError(service, atendee, failMessage, expectedExceptionMessage);
+
+        atendee.email = 'sdsdfa#gmail.com';
+        serviceHelper.tryCreateAtendeeWithError(service, atendee, failMessage, expectedExceptionMessage);
+
+        atendee.email = 'sdsdfa@gmail';
+        serviceHelper.tryCreateAtendeeWithError(service, atendee, failMessage, expectedExceptionMessage);
+
+    });
+
 
 });
