6da45f2474a721b7c6b675fcf45105f0d994b510
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-11-12 10:29:24 -0300

    Added burgos-agency

--- src/BurgosAgency/burgos-agency.ts
+++ src/BurgosAgency/burgos-agency.ts
@@ -1,3 +1,20 @@
 export class BurgosAgency {
-    
+    private static agencyName: string;
+    private static manager: string;
+
+    static GetName(): string {
+        return BurgosAgency.agencyName;
+    }
+
+    static SetName(name: string): void {
+        BurgosAgency.agencyName = name;
+    }
+
+    static GetManager(): string{
+        return BurgosAgency.manager;
+    }
+
+    static SetManager(name: string): void {
+        BurgosAgency.manager = name;
+    }
 }
