08460667d5cf5f73c61ddf6c92e3debfc38cb50f
Hercules Silva de Souza <hercules.silva@dcx.ufpb.br>
2019-11-13 13:27:47 -0300

    t011 - Implement Attendant Service. Waiting for Business Rules | Writing Antecedent Controller

--- src/atendee/atendee/atendee.controller.ts
+++ src/atendee/atendee/atendee.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put, Body } from '@nestjs/common';
+import { Atendee } from '../atendee.entity';
+import { AtendeeService } from './atendee.service';
 
+// url http://localhost:3000/atendee
 @Controller('atendee')
-export class AtendeeController {}
+export class AtendeeController {
+
+    // The service will take care of the logic
+    constructor(private antendeeService: AtendeeService) {
+    }
+
+    // end-point create
+    // http://localhost:3000/atendee/create
+    @Post('create')
+    async create(@Body() atendeeDate: Atendee): Promise<any> {
+        return await this.antendeeService.create(atendeeDate);
+    }
+
+    // @Put(':id/update')
+    // async update()
+
+
+}
--- src/atendee/atendee/atendee.service.ts
+++ src/atendee/atendee/atendee.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Atendee } from '../atendee.entity';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
+/**
+ * This class will be responsible for the logic of CRUD.
+ */
 @Injectable()
-export class AtendeeService {}
+export class AtendeeService {
+
+    constructor(
+        // The atendeeRepository will take care of CRUD 
+        @InjectRepository(Atendee)
+        private atendeeRepository: Repository<Atendee>
+    ) { }
+
+
+    // CREATE
+    async create(atendee: Atendee): Promise<any> {
+        return await this.atendeeRepository.save(atendee);
+    }
+
+    // READ
+    async findAll(): Promise<Atendee[]> {
+        return await this.atendeeRepository.find();
+    }
+
+
+    // UPDATE
+    async update(atendee: Atendee): Promise<UpdateResult> {
+
+        return await this.atendeeRepository.update(atendee.id, atendee);
+    }
+
+    // DELETE
+    async delete(id): Promise<DeleteResult> {
+        return await this.atendeeRepository.delete(id);
+    }
+}
