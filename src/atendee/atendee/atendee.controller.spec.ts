a6cc92333136341999978977eefce3abe8398495
Hercules Silva de Souza <hercules.silva@dcx.ufpb.br>
2019-11-14 10:53:08 -0300

    text_exchange - deleted last service, atendee.ts to atendee.entity.ts . Switching test mode

--- src/atendee/atendee/atendee.controller.spec.ts
+++ src/atendee/atendee/atendee.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AtendeeController } from './atendee.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Atendee } from '../atendee.entity';
 
 describe('Atendee Controller', () => {
   let controller: AtendeeController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Atendee])],
       controllers: [AtendeeController],
     }).compile();
 
--- src/atendee/atendee/atendee.service.spec.ts
+++ src/atendee/atendee/atendee.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AtendeeService } from './atendee.service';
+import { async } from 'rxjs/internal/scheduler/async';
 
 describe('AtendeeService', () => {
   let service: AtendeeService;
@@ -12,7 +13,25 @@ describe('AtendeeService', () => {
     service = module.get<AtendeeService>(AtendeeService);
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
     expect(service).toBeDefined();
   });
+
+  // it('', () => {
+
+  //   let atendee = new Atendee();
+  //   atendee.name = "Meytal"
+  //   atendee.email = "m@gmail.com"
+  //   atendee.ssn = "ss11";
+  //   atendee.date = new Date();
+
+  //   let createdAtendee: Promise<Atendee> = service.create(atendee);
+
+  //   expect(createdAtendee).toEqual(atendee);
+
+
+  //   // expect()
+
+  // });
+
 });
